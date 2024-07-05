24de98deffa6406718be3e147905bc3de9803331
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-07-05 19:29:10 +0200

    read package json

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,15 @@
-import { HttpServerRequest } from '@effect/platform';
+import { FileSystem, HttpServerRequest } from '@effect/platform';
+import { NodeFileSystem } from '@effect/platform-node';
 import type { MetaFunction } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { Effect } from 'effect';
 import { makeLoader } from 'remix-effect';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { Schema } from '@effect/schema';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const meta: MetaFunction = () => {
   return [
@@ -15,12 +22,20 @@ export const loader = makeLoader(
   Effect.gen(function* () {
     yield* Effect.logDebug('init / loader');
 
+    const fileSystem = yield* FileSystem.FileSystem;
+
     return Effect.gen(function* () {
       const request = yield* HttpServerRequest.HttpServerRequest;
+      const content = yield* fileSystem
+        .readFileString(__dirname + '/../../package.json')
+        .pipe(
+          Effect.flatMap(Schema.decode(Schema.parseJson(Schema.Object))),
+          Effect.orDie
+        );
 
-      return { url: request.url };
+      return { url: request.url, content };
     });
-  })
+  }).pipe(Effect.provide(NodeFileSystem.layer))
 );
 
 export default function Index() {
