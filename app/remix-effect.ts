61f593fcc14f2264e625c45a0d24e112b14f3a7e
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-07-08 10:27:47 +0200

    chore: cleanup docker files

--- app/remix-effect.ts
+++ app/remix-effect.ts
@@ -4,13 +4,14 @@ import { Effect, Layer, ManagedRuntime, Scope } from 'effect';
 import { NodeSdk } from '@effect/opentelemetry';
 import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import pckg from '../package.json';
 
 type AppContext = {
   getRequestId: () => string;
 };
 
 const NodeSdkLive = NodeSdk.layer(() => ({
-  resource: { serviceName: 'example' },
+  resource: { serviceName: pckg.name },
   spanProcessor: new BatchSpanProcessor(new OTLPTraceExporter()),
 }));
 
@@ -18,7 +19,7 @@ export class RemixArgs extends Effect.Tag('@app/services/RemixArgs')<
   RemixArgs,
   Omit<LoaderFunctionArgs, 'context'> & {
     ctx: AppContext;
-    requestId: Effect.Effect<string, never, never>;
+    requestId: string;
   }
 >() { }
 
@@ -38,28 +39,32 @@ export const makeRemixRuntime = <R>(layer: Layer.Layer<R, never, never>) => {
   ) => {
     const makeLoaderPromise = runtime.runPromise(body);
 
-    return ({ context, ...args }: LoaderFunctionArgs) => {
+    return async ({ context, ...args }: LoaderFunctionArgs) => {
       const ctx = context as AppContext;
 
-      return makeLoaderPromise.then((effect) =>
-        runtime.runPromise(
-          effect.pipe(
-            Effect.provideService(
-              RemixArgs,
-              RemixArgs.of({
-                ...args,
-                ctx,
-                requestId: Effect.sync(ctx.getRequestId),
-              })
-            ),
-            Effect.provideService(
-              HttpServerRequest.HttpServerRequest,
-              HttpServerRequest.HttpServerRequest.of(
-                HttpServerRequest.fromWeb(args.request)
-              )
-            ),
-            Effect.scoped
-          )
+      const requestId = ctx.getRequestId();
+
+      const effect = await makeLoaderPromise;
+
+      return runtime.runPromise(
+        effect.pipe(
+          Effect.provideService(
+            RemixArgs,
+            RemixArgs.of({
+              ...args,
+              ctx,
+              requestId,
+            })
+          ),
+          Effect.provideService(
+            HttpServerRequest.HttpServerRequest,
+            HttpServerRequest.HttpServerRequest.of(
+              HttpServerRequest.fromWeb(args.request)
+            )
+          ),
+          Effect.scoped,
+          Effect.withSpan('data-fn-handler'),
+          Effect.annotateSpans('req-id', requestId)
         )
       );
     };
